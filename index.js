27c5bcf77498e7d0ee28a4f82c831c89661c027a
Hariidev <hsugeen14@gmail.com>
2020-10-08 12:12:48 +0530

    Related products page added | products.html

--- index.js
+++ index.js
@@ -42,6 +42,29 @@ $("#new-arrival .owl-carousel").owlCarousel({
 });
 
 
+
+
+$("#products-main .owl-carousel").owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        600:{
+            items:3
+        },
+        1000:{
+            items:7
+        }
+    }
+});
+
+
+
+
 //Isotope Filter
  var $grid = $(".grid").isotope({
      itemSelector : '.grid-item',
